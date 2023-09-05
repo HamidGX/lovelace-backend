3218fd04ec9345222b8f2f8a4ec0394719b2729e
HamidGX <hamid.uxw@gmail.com>
2023-09-05 16:36:09 -0600

    Added CORS config

--- index.js
+++ index.js
@@ -9,20 +9,43 @@ import dalleRoutes from './routes/dalleRoutes.js'
 dotenv.config()
 
 const app = express()
-app.use(cors())
-app.use(express.json({ limit: '50mb' }))
+app.use(express.json())
 
-app.use('/api/v1/post', postRoutes)
-app.use('/api/v1/dalle', dalleRoutes)
+// Configurar CORS
+const whitelist = [process.env.FRONTEND_URL]
+
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.includes(origin)) {
+			// Puede consultar la API
+			callback(null, true)
+		} else {
+			// No esta permitido
+			const error = new Error('Error de Cors')
+			callback(error)
+		}
+	},
+}
 
 app.get('/', async (req, res) => {
-	res.send('Hello from Lovelace')
+	res.status(200).json({
+		message:
+			'Hola, bienvenido a la API de Lovelace. Esta API es privada, por lo que no se permite realizar consultas a esta misma.',
+	})
 })
 
-app.use(function (err, req, res, next) {
-	console.error(err.stack)
-	res.status(500).send('Something broke!')
+app.use(cors(corsOptions))
+// Manejar errores de CORS
+app.use((err, req, res, next) => {
+	if (err.message === 'Error de Cors') {
+		res.status(403).json({ error: 'Acceso no permitido desde este origen.' })
+	} else {
+		// Otro tipo de errores
+		res.status(500).json({ error: 'Ocurrió un error en el servidor.' })
+	}
 })
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/dalle', dalleRoutes)
 
 const startServer = async () => {
 	try {
